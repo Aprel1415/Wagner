a04f96de7b93233bddacf4297b30e8d866b1341d
Aprel <113014235+Aprel1415@users.noreply.github.com>
2023-10-31 23:03:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 var greetings = "hello to world!";
-console.log("Hey, bro! Say " + greetings + "!!")
+console.log("Hey, bro! Say " + greetings + "!!");
+var num_1 = 5;
+var num_2 = 2;
+var num_3 = 55;
+var res = num_2 + num_3;
+console.log("вычитание: " + (num_1 - num_2) + " - " + res + " = -54");
+console.log(Math.PI + ": Число ПИ");
+console.log(Math.min(4,1,2,22,5,61,77,0) + ": Минимальное число");
