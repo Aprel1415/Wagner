ed17b36aab164e2cb9e422e453643491b7787744
Artemy Aprelikov <aprel1415@yandex.ru>
2023-11-21 22:37:07 +0300

    плавная анимация замены картинки при наведении

--- main.js
+++ main.js
@@ -130,13 +130,21 @@ function clicker (el) {
     }
 } 
 */
-function slider () {
-    const img = document.getElementById("mainpic33");
-    img.src = "Лучший в Аду.jpg";
-}
 
-function sliderover () {
-    const img = document.getElementById("mainpic33");
-    img.src = "Приг.jpg";
-}
 
+function changeImage(element) {
+    element.classList.add("fade");
+    setTimeout(function() {
+      element.src = "Лучший в Аду.jpg";
+      element.classList.remove("fade");
+    }, 500);
+  }
+  
+  function restoreImage(element) {
+    element.classList.add("fade");
+    setTimeout(function() {
+      element.src = "Приг.jpg";
+      element.classList.remove("fade");
+    }, 500);
+    
+  }
